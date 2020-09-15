72d8d2c27aadd7e738d7a145d360331f12e932f9
Lebron <wenzhi.zeng@outook.com>
2020-09-15 23:48:49 +0800

    added App

---
+++ iOSCodeSnippets/Types/App.swift
@@ -0,0 +1,15 @@
+//
+//  App.swift
+//  iOSCodeSnippets
+//
+//  Created by 曾文志 on 2020/9/15.
+//  Copyright © 2020 Lebron. All rights reserved.
+//
+
+import Foundation
+
+enum App {
+    static let versionNumber = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    static let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+    static let currentVersionDescription = "\(versionNumber ?? "")(\(buildNumber ?? ""))"
+}
