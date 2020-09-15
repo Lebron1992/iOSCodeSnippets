911315da2def64fa6d1196632f4af1f977fb92b4
Lebron <wenzhi.zeng@outook.com>
2020-09-15 23:18:21 +0800

    Initial Commit

---
+++ iOSCodeSnippets/iOSCodeSnippets.h
@@ -0,0 +1,19 @@
+//
+//  iOSCodeSnippets.h
+//  iOSCodeSnippets
+//
+//  Created by 曾文志 on 2020/9/15.
+//  Copyright © 2020 Lebron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSCodeSnippets.
+FOUNDATION_EXPORT double iOSCodeSnippetsVersionNumber;
+
+//! Project version string for iOSCodeSnippets.
+FOUNDATION_EXPORT const unsigned char iOSCodeSnippetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSCodeSnippets/PublicHeader.h>
+
+
